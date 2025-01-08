a2251fac0e0da5f1ed2ad06718c90a1852506fc8
farhan-uddin-jibon <farhanuddinjibon@gmail.com>
2025-01-08 10:45:56 +0600

    final

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
 # Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --noinput
+
+# Run database migrations
+python manage.py migrate
